186055b3bf39c77e97049dbebd3aca74b079ec39
huangm5 <michael.huang44@gmail.com>
2022-03-08 09:08:49 -0500

    Signed-off-by: huangm5 <michael.huang44@gmail.com>

--- trump2024.py
+++ trump2024.py
@@ -1,12 +1,13 @@
-#keep configable from sikuli
+#manage holidays
 #to allow merge tool
 #to allow multiple scenario
+#keep configable from sikuli /ok /revisit 1 month 3/28
+#2D facets to 3D finding stories
+#freq visit route
+
 gtx1060="gtx1060";
 inossem="inossem";
-scenario=inossem;
-#2D facets to 3D finding stories
-#valuntine
-#freq visit route
+scenario=gtx1060;
 
 global zoo,landmarks,Orig_Xs,Orig_Ys;
 global lastLoc;
@@ -25,7 +26,34 @@ ClientRegions={
         inossem:Region(1,100,1200,625)
         };
 ClientRegion=ClientRegions[scenario];
-LocCenter=ClientRegion.getCenter();#Location(933, 503);//screen select region //can capture!
+LocCenter=ClientRegion.getCenter();#Location(933, 503);//screen select region //can capture! 
+
+def which_zoo_gtx1060():
+    zoo='unknown';
+    try:
+        zoc=Region(249,75,38,71).find("1646455711214.png");
+        zoo="Main";
+        return zoo;
+    except:
+        pass;
+    try:
+        zoc=Region(249,75,38,71).find("1646455529709.png");
+        zoo="Fir";
+        return zoo;
+    except:
+        pass;
+    try:
+        zoc=Region(249,75,38,71).find("1646455590532.png");
+        zoo="Terrarium";
+        return zoo;
+    except:
+        pass;   
+    return zoo;
+zoo=which_zoo_gtx1060();
+print(zoo);
+SetDestFir=0;
+SetDestTerrarium=0;
+
 def my_friend_gonext():
     mouseMove(Location(489, 580)); 
     mouseDown(Button.LEFT);
@@ -224,16 +252,22 @@ def  SetCenterEstimate(x,y):
 #print( Region(174,49,1571,978).find());    
 Main="Main";
 Fir="Fir";
-landmarkss={gtx1060:{
-        Main:["1640721586941.png","1640719028561.png","1640720333764.png","1644708172971.png","1640720057152.png","1640721817026.png","1640721745982.png","1640741694803.png","1640744943976.png","1641391579681.png","1641394421693.png","1640536842754.png"]
-        ,
-        Fir:["1641135019722.png","1641135048732.png","1641135072434.png","1640640250064.png"]
+Terrarium="Terrarium";
+landmarkss={
+        gtx1060:{
+            Main:["1640721586941.png","1640719028561.png","1640720333764.png","1644708172971.png","1640720057152.png","1640721817026.png","1640721745982.png","1640741694803.png","1640744943976.png","1641391579681.png","1641394421693.png","1640536842754.png"]
+            ,
+            Fir:["1641135019722.png","1641135048732.png","1641135072434.png","1640640250064.png"]
+            ,
+            Terrarium:["1641135019722.png","1641135048732.png","1641135072434.png","1640640250064.png"]
         },
         inossem:{
-        Main:["1640721586941.png","1640719028561.png","1640720333764.png","1644708172971.png","1645764608542.png"
-                 ,"1640721817026.png","1640721745982.png","1640741694803.png","1640744943976.png","1641391579681.png","1641394421693.png","1640536842754.png"]
-        ,
-        Fir:["1641135019722.png","1641135048732.png","1641135072434.png","1640640250064.png"]
+            Main:["1640721586941.png","1640719028561.png","1640720333764.png","1644708172971.png","1645764608542.png"
+                     ,"1640721817026.png","1640721745982.png","1640741694803.png","1640744943976.png","1641391579681.png","1641394421693.png","1640536842754.png"]
+            ,
+            Fir:["1641135019722.png","1641135048732.png","1641135072434.png","1640640250064.png"]
+            ,
+            Terrarium:["1641135019722.png","1641135048732.png","1641135072434.png","1640640250064.png"]
         }}[scenario];
 #Region(302,139,1431,864).find(landmarkss[Main][3]);
 zoo=Main;
@@ -241,7 +275,8 @@ Orig_XsMain  =[0       ,-7     ,2      ,3        ,-90   ,523    ,1988   ,800    
 Orig_YsMain  =[0       , 0     ,2      ,3       ,-17   ,215    , 827   ,-110   ,30    ,-1500                       ,-2000     ,-200,0,0,0];
 Orig_XsFir  =[0       ,27     ,36         ,638,0,0,0,0,0];
 Orig_YsFir  =[0       , -20     ,-100     ,418,0,0,0,0,0];
-Orig={ Main:{
+Orig={
+        gtx1060:{ Main:{
             "x":[0       ,-7     ,2      ,3        ,590   ,523    ,1988   ,800    ,-500  ,20000                       ,18000     ,102,0,0,0],
             "y":[0       , 0     ,2      ,3       ,17   ,215    , 827   ,-110   ,30    ,-1500                       ,-2000     ,-200,0,0,0]       
             },
@@ -249,7 +284,17 @@ Orig={ Main:{
             "x":[0       ,27     ,36         ,638,0,0,0,0,0],
             "y":[0       , -20     ,-100     ,418,0,0,0,0,0] 
             }
-     };
+     },
+        inossem:{ Main:{
+            "x":[0       ,-7     ,2      ,3        ,590   ,523    ,1988   ,800    ,-500  ,20000                       ,18000     ,102,0,0,0],
+            "y":[0       , 0     ,2      ,3       ,17   ,215    , 827   ,-110   ,30    ,-1500                       ,-2000     ,-200,0,0,0]       
+            },
+       Fir :{
+            "x":[0       ,27     ,36         ,638,0,0,0,0,0],
+            "y":[0       , -20     ,-100     ,418,0,0,0,0,0] 
+            }
+     }
+        }[scenario];
 Orig_Xs=Orig[zoo]["x"];
 Orig_Ys=Orig[zoo]["y"];
 landmarks=landmarkss[Main];
@@ -259,10 +304,50 @@ ScanLandmarksRegions={
         inossem:Region(120,185,993,521)
         }[scenario];
 
+
+
+fls=os.listdir(getBundlePath()+"/"+scenario);
+fls=filter(lambda x: (x[:3]=="tmp" and x[-4:]==".png"),fls);
+gls=map(lambda x: {"g":x[3:-4].split("_"),"x":x},fls);
+#print(tuple(gls)); # 33s
+import json;
+class obj(object):
+    def __init__(self, d):
+        for a, b in d.items():
+            if isinstance(b, (list, tuple)):
+               setattr(self, a, [obj(x) if isinstance(x, dict) else x for x in b])
+            else:
+               setattr(self, a, obj(b) if isinstance(b, dict) else b)
+
+tls=map(lambda y:({"sz":int(y["g"][0]),"x":int(y["g"][1]),"y":int(y["g"][2])
+            ,"pic":y["x"]}),gls);
+ols=map(lambda z:obj(z),tls);
+#print (tls[1].sz);
+#print(json.dumps(tls[1])); 
+#print(json.dumps(tls)); # 33s
+#print(ols[1].sz);
+def TooManyNoneOverlap(matches,w) :
+    for i in range(len(matches)): 
+        if i>=len(matches):
+              break;
+        if i>2: 
+            #dup
+              break;
+        m=matches[i];
+        for j in range(i+1,len(matches)): 
+          if j>=len(matches):
+              break;
+          g=matches[j];
+          if abs(m.x-g.x)<w and abs(m.y-g.y)<w :
+              print(m,g);
+              del matches[j];
+              j-=1;
+
 def ScanLandmarks():
     global landmarks,Orig_Xs,Orig_Ys;
     i = 0;
     loc0=Location(0,0);
+    found=0;
     while i < len(landmarks):
         try: 
             loc= ScanLandmarksRegions.find(landmarks[i]);
@@ -270,6 +355,7 @@ def ScanLandmarks():
             i+=1;
             continue;
         freqmarks[i]+=1;
+        found=1;
         Log((zoo+" Landmark[{I},{X},{Y},{N}]={L}-maybe-[{X0},{Y0}]").format(X=Orig_Xs[i],Y=Orig_Ys[i]
                     ,I=i,L=loc,N=freqmarks[i]
                     ,X0=CenterX+loc.x-LocCenter.x,Y0=CenterY+loc.y-LocCenter.y));# Do not use > in Log
@@ -286,7 +372,60 @@ def ScanLandmarks():
             Orig_Ys[i]=loc.y+CenterY;
             Log('L[{X},{Y}], {I} BY GUESS'.format(X=Orig_Xs[i],Y=Orig_Ys[i],I=i));
         i += 1  ;  
-        
+    if found==0 :
+        Log("Not found any confirmed near[{X},{Y}]".format(X=CenterX,Y=CenterY));
+        if(CenterX==0 and CenterY==0) :
+           xx=0;
+           for x in ols:
+               try :
+                   matches=ClientRegion.findAllList(scenario+"/"+x.pic);
+                   TooManyNoneOverlap(matches,x.sz);
+                   loc=matches[0].getTarget();
+                   Log("Found {P} @ {L}".format(P=x.pic,L=loc));
+                   if len(matches)>=2 :
+                       Log("But Too Many"); 
+                   xx=x;
+                   SetCenterEstimate(xx.x-loc.x+LocCenter.x,xx.y-loc.y+LocCenter.y);        
+                   break;
+               except:
+                   pass;        
+           if(xx==0):
+                Log("Not found any candidate near[{X},{Y}]".format(X=CenterX,Y=CenterY));
+        else:
+            ols.sort(key=lambda x: abs(x.x-CenterX)+abs(x.y-CenterY));
+            Log("Nearest [{X},{Y}] {P} ".format(X=ols[0].x,Y=ols[0].y,P=ols[0].pic));
+            try:            
+                loc=ClientRegion.find(scenario+"/"+ols[0].pic);
+                Log("Found @ {L}".format(L=loc));
+                return;
+            except:
+                Log("Not found, try second ...");                
+                for i in range(1,10):                
+                    try:
+                        loc=ClientRegion.find(scenario+"/"+ols[i].pic);                
+                        Log("Found {P} @ {L}".format(P=ols[i].pic,L=loc));
+                        return;
+                    except:
+                        pass;
+            sc=clickRange.getScreen();
+            for w in range(6,43):
+                offsetX=random.randrange(-100,100);
+                offsetY=random.randrange(-80,80);
+                img=sc.capture(LocCenter.x-w+offsetX,LocCenter.y-w+offsetY,2*w,2*w);#.png
+                pngName="tmp{W}_{X}_{Y}.png".format(W=w,X=CenterX+offsetX,Y=CenterY+offsetY);
+                img.save(getBundlePath()+"/"+scenario,pngName);
+                matches=clickRange.findAllList(scenario+"/"+pngName);
+                TooManyNoneOverlap(matches,w);
+                if len(matches)<2 :
+                    break;
+                os.remove(getBundlePath()+"/"+scenario+"/"+pngName);
+    #            [error] OSError ( unlink(): an unknown error occurred: C:\Users\Admin\Documents\trump2024.sikuli\gtx1060\tmp13_2664_408.png )
+            print(w);
+            for m in matches: 
+                print(m);
+            
+                  
+
 ScanLandmarks();
 
 clickRegions={
@@ -335,10 +474,11 @@ def my_Fir():
         r=my_click("1640640105756.png")    ;    
         sleep(3);
     return r;
-def my_Terrarium():   
-    r=my_click(landmarksFir[3])    ;
+def my_Terrarium(station):   
+    r=my_click(station)    ;
     r=my_click("1640640267632.png")    ;    
-    sleep(3);
+    sleep(5);
+    wheel(LocCenter,Button.WHEEL_DOWN,10);
     return r;
 
 import random;
@@ -459,11 +599,12 @@ def my_feed():
 def my_water():
     global lastLoc;
     try:    
-        loc={ 
-        gtx1060: Region(352,167,1185,721)
+        work={ 
+        gtx1060:[Region(439,216,1083,712),"water.png"]
                 ,
-                inossem:clickRange.grow(-50)
-                }[scenario].find(inossem+'/'+"water.png").getTarget();
+                inossem:[clickRange.grow(-50),inossem+"water.png"]
+                }[scenario];
+        loc=work[0].find(work[1]).getTarget();
         drag(loc);
         sleep(0.1);
         mouseMove(loc.offset(100,80));
@@ -479,46 +620,84 @@ def my_water():
         return 0;
     except:
         return 999;
-my_water();
+#my_water();
+
+woverh=30;
+trail=[ 
+        [woverh,0,0.1],
+        [-woverh-10,0,0.1],
+        [-woverh-10,-10,0.1],
+        [woverh+10,-10,0.1],
+        [woverh+10,10,0.1],
+        [-woverh-20,10,0.1],
+        [-woverh-20,-20,0.1],
+        [woverh+20,-20,0.1],
+        [woverh+20,+20,0.1],
+        [-woverh-30,+20,0.1],
+        [-woverh-30,-30,0.1],
+        [woverh+30,-30,0.1],
+        [woverh+30,+30,0.1],
+        [-woverh-40,+30,0.1],
+        [-woverh-40,-40,0.1],
+        [woverh+40,-40,0.1],
+        [woverh+40,+40,0.1],
+        [-woverh-50,+40,0.1],
+        [-woverh-50,-50,0.1],
+        [woverh+50,-50,0.1]
+        ];
 
 def my_poo():
     global lastLoc;
     try:    
         loc= Region(340,186,1294,772).find("1640315967731.png").getTarget();
-        mouseMove(loc);
-        mouseDown(Button.LEFT);
-        sleep(0.3);
-        mouseUp(Button.LEFT);
-        sleep(2);
-        lastLoc=loc;    
+        loc0=loc;
     except:
-        return 999;
-    
-    Region(601,907,150,112).find("1640316099386.png");        
-    loc= Region(219,96,1482,923).find("1640317826765.png").getTarget();
+        return 888;
     mouseMove(loc);
-    lastLoc=loc;    
-    
-    fruits = ["1640316288074.png", "1640317465662.png","1640317490156.png","1640317623212.png","1640317634822.png"]
-    loc2=Location(0,0);
-    for x in fruits:
-        try:   
-             loc2= Region(317,108,1345,828).find(x).getTarget();
-             print(x);
-             break;
-        except:
-            pass;
-    if (loc2==Location(0,0)) :
-       return 999;
     mouseDown(Button.LEFT);
     sleep(0.3);
-    mouseMove(loc2);    
-    sleep(0.3);
     mouseUp(Button.LEFT);
-    sleep(1);
-    lastLoc=loc2;    
-    return 0;
-
+    lastLoc=loc;    
+    sleep(2);
+    wheel(LocCenter,Button.WHEEL_DOWN,10);
+    try:
+        loc= Region(219,96,1482,923).find("1640317826765.png").getTarget();
+    except:
+        Log("Missingg poo brush!");
+        return 999;
+    mouseMove(loc);
+    mouseDown(Button.LEFT);
+    sleep(0.1);
+    for t in trail:
+        lastLoc=Location(t[0]*8.5+LocCenter.x,t[1]*7+LocCenter.y);
+        mouseMove(lastLoc);    
+        sleep(t[2]);
+#    mouseUp(Button.LEFT);
+    sleep(0.3);
+#    mouseDown(Button.LEFT);
+    for i in range(1,5):
+        for j in range(1,3):
+            try:
+                loc= ClientRegion.find("1640317826765.png").getTarget();
+                break;
+            except:
+                sleep(0.2);
+                pass;
+        if j>=4 :
+            Log("Done poo brush");
+            mouseUp(Button.LEFT);
+            sleep(1);
+            return 0;
+        cl=ClientRegion.grow(-30);
+        mouseMove(Location(random.randrange(cl.x,cl.x+cl.w),
+                            random.randrange(cl.y,cl.y+cl.h))
+                    );                
+        sleep(0.1);
+    Log("Failed poo brush {L}".format(L=loc0));
+    mouseUp(Button.LEFT);
+    return 999;
+my_poo();
+                    
 def merge_regions(regions):
     x=min(map(lambda x:x.x,regions));
     y=min(map(lambda x:x.y,regions));
@@ -685,7 +864,10 @@ def my_first_try():
         sleep(0.1);
         dropAt(Location(1618, 618));        
 #my_first_try();
-
+def ispoppydays(now):
+    return (now.month==2 and now.day>=14 and now.day<=14+14 
+        or now.month==4 and now.day>=17 and now.day<=17+14 );
+            
 import datetime
 now = datetime.datetime.now()
 try:
@@ -700,12 +882,17 @@ try:
 except:
     running=-1;
 mPoppyLast=0;    
+last_fail_poo=now;
 lastStatus='init';
 while 1==1 :
-    zoo='Main';        
-    landmarks=landmarkss[Main];
-    Orig_Xs=Orig_XsMain;
-    Orig_Ys=Orig_YsMain;
+    zoo=which_zoo_gtx1060();
+    Log(zoo);
+    if(zoo=='unknown') :
+       zoo='Main';     
+    landmarks=landmarkss[zoo];
+
+    Orig_Xs=Orig[zoo]['x'];
+    Orig_Ys=Orig[zoo]['y'];
     
     now = datetime.datetime.now();
     #print(now.year, now.month, now.day, now.hour, now.minute, now.second)
@@ -725,7 +912,10 @@ while 1==1 :
     NLOOP=500;#1000;
     first_try=1;
     startTime = datetime.datetime.now();   
-    poppy=0;SetRampDest(100,100);                
+    if ispoppydays(now) :
+        #Monday, February 14 valentine
+        #Sunday, April 17 easter
+        poppy=0;SetRampDest(100,100);                
     displaceCount=0;    
     while i<NLOOP:
         now = datetime.datetime.now();
@@ -763,7 +953,7 @@ while 1==1 :
         if(my_close()==0) : #>star
            lastStatus='close';
            continue;
-        #my_first_try();
+#my_first_try();
         if(my_star()==0) :
            lastStatus='star';
            continue;
@@ -788,28 +978,29 @@ while 1==1 :
         if(my_click("1640623363794.png")==0):
             lastStatus='breed';
             continue;
-        if(poppy<0) :
-            try:
-               Region(178,177,126,806).find("1644682415730.png"); 
-               poppy=0;        
-            except:
-               pass;
-        if(my_click("1644338915756.png")==0):
-           poppy+=1;
-           SetRampDest(0,0);
-           lastStatus='poppy';
-           continue;
-        PoppyCenterX=22;
-        PoppyCenterY=50;        
-        if(CenterX<PoppyCenterX+50 and CenterX>PoppyCenterX-50 and CenterY>PoppyCenterY-50 and CenterY<PoppyCenterY+50):
-            SetRampDest(0,0);
-            Log('All poppy gone');
-        mPoppy=int(now.minute/6);
-        #Log('Poppy Minuter={m}'.format(m=mPoppy));
-        if(mPoppy!=mPoppyLast):
-            mPoppyLast=mPoppy;
-            SetRampDest(PoppyCenterX,PoppyCenterY);
-            Log('Where is poppy?');
+        if ispoppydays(now):
+            if(poppy<0) :
+                try:
+                   Region(178,177,126,806).find("1644682415730.png"); 
+                   poppy=0;        
+                except:
+                   pass;
+            if(my_click("1644338915756.png")==0):
+               poppy+=1;
+               SetRampDest(0,0);
+               lastStatus='poppy';
+               continue;
+            PoppyCenterX=22;
+            PoppyCenterY=50;        
+            if(CenterX<PoppyCenterX+50 and CenterX>PoppyCenterX-50 and CenterY>PoppyCenterY-50 and CenterY<PoppyCenterY+50):
+                SetRampDest(0,0);
+                Log('All poppy gone');
+            mPoppy=int(now.minute/6);
+            #Log('Poppy Minuter={m}'.format(m=mPoppy));
+            if(mPoppy!=mPoppyLast):
+                mPoppyLast=mPoppy;
+                SetRampDest(PoppyCenterX,PoppyCenterY);
+                Log('Where is poppy?');
         if(my_friend()==0):
            lastStatus='friend';
            continue;
@@ -825,20 +1016,28 @@ while 1==1 :
         if(my_click("1640869544082.png")==0):
            lastStatus='check';
            continue;
-        if(zoo=='MainXXX' and i>160*NLOOP/400) :#80
-            SetRampDest(100,20); #'Garage'
-            if(poppy>0):
-                if(my_Fir()==0):            
-                    zoo='Fir';
-                    landmarks=landmarksFir;
+        if(last_fail_poo+ datetime.timedelta(minutes=1) <now): 
+            if(my_poo()==999):
+                last_fail_poo=now;
+        if not ispoppydays(now) and zoo=='Main' and i>160*NLOOP/400 :#80
+            if SetDestFir:
+                SetDestFir=1; # do not set again even failed ramping
+                SetRampDest(100,20); #'Garage'
+            if(my_Fir()==0):            
+                    zoo='Fir';                
+                    landmarks=landmarkss[zoo];
                     Orig_Xs=Orig_XsFir;
                     Orig_Ys=Orig_YsFir;                
                     SetRampDest(0,0);
                     continue;
-        if(zoo=='FirXZZ' and i>250*NLOOP/400) :
-            SetRampDest(Orig_XsFir[3],Orig_YsFir[3]); #station
-            if(my_Terrarium()==0):            
+        if((zoo=='Fir' or zoo=='Main') and not ispoppydays(now) and i>250*NLOOP/400) :
+            station=obj({"x":Orig_XsFir[3],"y":Orig_YsFir[3],"p":landmarkss[Fir][3]});
+            if SetDestTerrarium:
+                SetDestTerrarium=1; # do not set again even failed ramping
+                SetRampDest(station.x,station.y); #
+            if(my_Terrarium(station.p)==0):            
                 zoo='Terrarium'; #cannot handle feed by accident
+                landmarks=landmarkss[zoo];
                 SetRampDest(0,0);
                 continue;
             
